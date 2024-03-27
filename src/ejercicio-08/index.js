c4bc7fa9677fa35caea6ce871424b0e8ae219436
Jame5JM <jamesjmann@gmail.com>
2024-03-27 12:09:57 +0100

    Añadir el ejercicio  08

--- src/ejercicio-08/index.js
+++ src/ejercicio-08/index.js
@@ -1,1 +1,8 @@
-// Escribe tu código aquí:
+// Ejercico 8 - Verificar Inclusión
+const phrase = 'JavaScript es divertido'
+const searchTerm = 'divertido'
+const containsTerm = phrase.includes(searchTerm)
+
+console.log(containsTerm) // Resultado esperado: true
+
+
