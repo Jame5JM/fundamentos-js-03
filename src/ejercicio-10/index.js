784e264d253a9ee6167c384216e6b70f839d36f7
Jame5JM <jamesjmann@gmail.com>
2024-03-27 12:24:51 +0100

    Añadir el ejercicio 10

--- src/ejercicio-10/index.js
+++ src/ejercicio-10/index.js
@@ -1,1 +1,7 @@
-// Escribe tu código aquí:
+// Ejercicio 10 - Convertir a Cadena
+const number = 42
+const numberString = number.toString()
+
+console.log(numberString) // Resultado esperad: "42"
+
+
