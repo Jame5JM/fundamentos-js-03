d980ddd157eedcd3e2ecc285ce4cebcaad0b03f0
Jame5JM <jamesjmann@gmail.com>
2024-03-27 12:16:00 +0100

    Añadir el ejercicio  09

--- src/ejercicio-09/index.js
+++ src/ejercicio-09/index.js
@@ -1,1 +1,6 @@
-// Escribe tu código aquí:
+// Ejercicio 9 - Eliminar Espacios
+const text = '   Hola, mundo   '
+const trimmedText = text.trim()
+
+console.log(trimmedText) // Resultado esperado: "Hola, mundo"
+
